12e3b1fe77ff9ce53fdfe3cfb038e7e8a47fced8
rafamaiadev <rafa.maia27@hotmail.com>
2023-02-24 20:31:54 -0300

    criei o json dos candidatos

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Chico',
+                partido: 'ABC',
+                fotos:[
+                {url:'38111.jpg', legenda: 'Vereador'}
+            ]
+            },
+            {
+                numero: '77222',
+                nome: 'Antonio',
+                partido: 'DEF',
+                fotos: [
+                {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Fabio',
+                partido: 'ABC',
+                vice: 'Tonho',
+                fotos: [
+                {url: '99.jpg', legenda: 'Prefeito'},
+                {url: '99_2.jpg', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Manel',
+                partido: 'HIJ',
+                vice: 'Augusto',
+                fotos:[
+                {url:'84.jpg', legenda: 'Prefeito'},
+                {url:'84_2.jpg', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+        ]
+    }
+];
